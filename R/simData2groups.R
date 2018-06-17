c12cd43c69aac5a79b3d1560f299e4ed5411f7ee
bdsegal <bdsegal@umich.edu>
2018-06-17 18:46:45 -0400

    added data files

---
+++ R/simData2groups.R
@@ -0,0 +1,14 @@
+#' Simulated data
+#'
+#' A dataset containing simulated repeated measures data
+#'
+#' @format A data frame with 900 rows of 4 variables:
+#' \describe{
+#'  \item{x}{covariate (could be time)}
+#'  \item{id}{subject id (clustering variable)}
+#'  \item{y}{response}
+#'  \item{group}{binary group indicator}
+#' }
+# @source \url{https://github.com/bdsegal/code-for-psplinesl1-paper}
+
+"simData2groups"
